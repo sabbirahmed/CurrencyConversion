2913a119e70bafd6d1ad1d6b1f41907afb15eb1a
Sabbir Ahmed <sabbir.cn@gmail.com>
2022-08-02 12:52:06 +0600

    Initial Commit

---
+++ CurrencyConversion/CurrencyConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConversionApp.swift
+//  CurrencyConversion
+//
+//  Created by Sabbir Ahmed on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
